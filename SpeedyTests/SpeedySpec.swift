52285e14dd2e8b826ab1730a073a90aa231e46da
Dirk Gadsden <dirk@esherido.com>
2015-08-21 22:50:57 -0700

    Refactor spec for hooks

--- SpeedyTests/SpeedySpec.swift
+++ SpeedyTests/SpeedySpec.swift
@@ -4,16 +4,6 @@ class SpeedySpec: Spec {
   func spec() {
     describe("Group") {
       describe("having a current group") {
-        var beforeEachCount = 0
-        var afterEachCount  = 0
-
-        beforeEach {
-          beforeEachCount += 1
-        }
-        afterEach {
-          afterEachCount += 1
-        }
-
         it("should allow defining an Example") {
           let original = getCurrentGroup()
           let testGroup = Group("a group")
@@ -41,15 +31,51 @@ class SpeedySpec: Spec {
           // Restore the original after we're done
           setCurrentGroup(original)
         }
+      }
+    }// describe Group
 
+    describe("Hooks") {
+      var beforeEachCount = 0
+      var afterEachCount  = 0
+      var beforeAllCount  = 0
+      var afterAllCount   = 0
+
+      describe("having a hooks testing group") {
+        beforeEach {
+          beforeEachCount += 1
+        }
+        afterEach {
+          afterEachCount += 1
+        }
+        beforeAll {
+          beforeAllCount += 1
+        }
+        afterAll {
+          afterAllCount += 1
+        }
+
+        it("will run some hooks") {
+          return
+        }
+      }
+
+      describe("having had a group with hooks") {
         it("should have called the beforeEach the correct number of times") {
-          expect(beforeEachCount).to(equal(2))
+          expect(beforeEachCount).to(equal(1))
         }
 
         it("should have called the afterEach the correct number of times") {
-          expect(afterEachCount).to(equal(2))
+          expect(afterEachCount).to(equal(1))
+        }
+
+        it("should have called the beforeAll the correct number of times") {
+          expect(beforeAllCount).to(equal(1))
+        }
+
+        it("should have called the afterAll the correct number of times") {
+          expect(afterAllCount).to(equal(1))
         }
       }
-    }
+    }// describe Hooks
   }
 }
